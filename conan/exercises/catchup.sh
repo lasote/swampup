e9d1dc2603942549ec994bc3c27410682bd49194
Luis Martinez de Bartolome <lasote@gmail.com>
2017-05-10 10:25:19 +0200

    some fixes

--- conan/exercises/catchup.sh
+++ conan/exercises/catchup.sh
@@ -4,9 +4,9 @@ RED='\033[0;41;30m'
 STD='\033[0;0;39m'
 APIKEY='AKCp2WWshJKjZjguhB3vD2u3RMwHA7gmxWUohWVhs1FqacHBAzKaiL2pp24NNUEhWHm5Dd4JY'
 
-exercise1() {
-   echo "performing exercise 1"
-   cd consumer
+exercise2() {
+   echo "performing Excercise 2 (consumer, with CMake)"
+   cd 1_consumer
    conan remote add artifactory http://35.185.192.7/artifactory
    mkdir -p build
    cd build
@@ -17,13 +17,14 @@ exercise1() {
    ./timer
 }
 
+
 read_options(){
         local choice
         read -p "Enter choice [ 1 - 4] " choice
         case $choice in
-                1) exercise1 ;;
+                1) exercise2 ;;
                 2) exercise2 ;;
-                3) exrecise3 ;;
+                3) exercise3 ;;
                 4) exit 0 ;;
                 *) echo -e "${RED}Error...${STD}" && sleep 2
         esac
@@ -34,10 +35,10 @@ show_menus() {
         echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
         echo " Automation Catch Up Menu "
         echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        echo "1. Excercise 1"
-        echo "2. Excercise 2"
-        echo "3. Excercise 3"
-        echo "4. Exit"
+        echo "2. Excercise 2 (consumer, with CMake)"
+        echo "3. Excercise 3 (consumer, change build_type)"
+        echo "4. Excercise 4 (consumer, change generator to gcc)"
+        echo "5. Exit"
 }
 
 while true
